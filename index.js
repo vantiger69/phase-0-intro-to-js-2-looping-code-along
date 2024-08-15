cd1eaf2b42e52e96510638e03391fec932a5002a
Titus kipchirchir <vantiger817@tgmail.com>
2024-08-15 14:30:52 +0300

    complete_lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solutions in this file
+//const gifts = ["teddy bear", "drone", "doll"];
+
+
+  ////  for (let i = 0; i < gifts.length; i++){
+    // console.log(`wrapped ${gifts[i]} and added a bow!`);
+   //     debugger;
+    //}
+    //return gifts;
+
+//wrapGifts(gifts);
+ 
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++){
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(message);
+    }
+    return messages;
+}
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+const thankYouMessages = writeCards(names, event);
+console.log(thankYouMessages);
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    let i = 3;
+    while (i > 0) {
+        console.log(`wrapped ${gifts[i-1]} and added a bow!`);
+        i--;
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--
+    }
+}
+countDown(10);
